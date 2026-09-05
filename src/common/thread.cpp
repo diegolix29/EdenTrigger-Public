c9795ad6ced25e8c78d54ae7b76da7b4b5a7f67b
Diego Mugetsu <diegoavilafacebook75@gmail.com>
2026-09-05 14:03:10 -0600

    JH

--- src/common/thread.cpp
+++ src/common/thread.cpp
@@ -649,41 +649,4 @@ void PinCurrentThreadToCore(size_t core_id) {
     }
 }
 
-void PinCurrentThreadToPerformanceCore(size_t core_id) {
-#if defined(__ANDROID__)
-    CpuTopologyState& state = State();
-    std::scoped_lock lock{state.topology_mutex};
-    EnsureTopologyLocked(state);
-
-    if (!state.separated) {
-        // If topology is not separated, just pin to the specific core
-        PinCurrentThreadToCore(core_id);
-        return;
-    }
-
-    // Pin to the specific core within the performance set
-    cpu_set_t set;
-    CPU_ZERO(&set);
-    CPU_SET(core_id, &set);
-
-    // Ensure the core is in the performance set
-    if (CPU_ISSET(core_id, &state.performance)) {
-        sched_setaffinity(pthread_self(), sizeof(set), &set);
-    } else {
-        // If the requested core is not a performance core, pin to the first performance core
-        for (s32 cpu = 0; cpu < CPU_SETSIZE; ++cpu) {
-            if (CPU_ISSET(cpu, &state.performance)) {
-                CPU_ZERO(&set);
-                CPU_SET(cpu, &set);
-                sched_setaffinity(pthread_self(), sizeof(set), &set);
-                break;
-            }
-        }
-    }
-#else
-    // On non-Android platforms, just pin to the specific core
-    PinCurrentThreadToCore(core_id);
-#endif
-}
-
 } // namespace Common
